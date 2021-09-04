f1055c289336d3fc1df42291f543eb18f0c467d7
hanahagewanko <237s10mimi@gmail.com>
2021-09-05 05:12:16 +0900

    feat: add vue && some components

---
+++ resources/js/components/TaskListComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <table class="table table-hover">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Content</th>
+                <th scope="col">Person In Charge</th>
+                <th scope="col">Show</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="i in 3" :key="i">
+                <th scope="row">{{i}}</th>
+                <td>Title1</td>
+                <td>Content{{i}}</td>
+                <td>Ichiro</td>
+                <td>
+                    <button class="btn btn-primary">Show</button>
+                </td>
+                <td>
+                    <button class="btn btn-success">Edit</button>
+                </td>
+                <td>
+                    <button class="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
